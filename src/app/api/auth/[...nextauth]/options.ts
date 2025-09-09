ee84e5f3db1c587f1ffdaf91cc9d31815ecbefe3
Husainj <jhalodwalahusain30@gmail.com>
2025-09-09 11:04:10 +0530

    middleware , pages , session , jwt and AuthProvider setup

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,5 @@
 import { NextAuthOptions } from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
@@ -11,8 +11,75 @@ import UserModel from "@/model/User";
 export const authOptions: NextAuthOptions ={
     providers : [
         CredentialsProvider({
-            id: "domain-login",
-            name: "Domain Account"
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email : {label : "Email" , type : "text"},
+                password : {label : "Password" , type: "password"}
+            },
+            async authorize(credentials: any): Promise<any>{
+                await dbConnect()
+                
+                try {
+                    console.log("111111111111111111 CREDENTIALS OBJECT :: " , credentials)
+                    const user = await UserModel.findOne({
+                        $or : [
+                            {email : credentials.identifier},
+                            {username : credentials.identifier}
+                        ]
+                    })
+
+                    if(!user){
+                        throw new Error('No user found with this email')
+                    }
+
+                    if(!user.isVerified){
+                        throw new Error('Please verify your account before login')
+                    }
+
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password , user.password)
+                    
+                    if(isPasswordCorrect){
+                        return user;
+                    }else{
+                        throw new Error('Incorrect password')
+                    }
+                    
+                } catch (err : any) {
+                    throw new Error(err)
+                }
+            }
         })
-    ]
+    ],
+    callbacks:{
+        async jwt({token , user}){
+            if(user){
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages = user.isAcceptingMessages
+                token.username = user.username
+            }
+
+            return token
+        },
+        async session ({session , token}){
+
+            if(token){
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
+
+
+            return session
+        }
+    },
+    pages:{
+        signIn: '/sign-in'
+    },
+    session :{
+        strategy : "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET
 }
